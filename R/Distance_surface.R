fadc5d5bef9c483657e7e2335365e7cf582f61e1
MirzaCengic <mirzaceng@gmail.com>
2018-05-29 11:59:47 +0200

    Intermediate files are deleted now

--- R/Distance_surface.R
+++ R/Distance_surface.R
@@ -31,7 +31,7 @@
 #' @importFrom fs file_temp path_ext_set file_delete
 #' @importFrom dplyr filter select pull
 #'
-get_distance_surface <- function(species_range, output_name, processing_resolution_data)
+get_distance_surface <- function(species_range, output_name, processing_resolution_data, keep = FALSE)
 {
   proc_resolution <- processing_resolution_data %>%
     dplyr::filter(species_name == species_range$binomial) %>%
@@ -51,7 +51,8 @@ get_distance_surface <- function(species_range, output_name, processing_resoluti
   ## Load realm and continent rasters
   if (!file.exists(output_name))
   {
-
+    file.remove(output_name)
+    file.remove(fs::path_ext_set(output_name, "sdat"))
 
     species_range_raster <- fasterize::fasterize(species_range, raster_mask)
 
